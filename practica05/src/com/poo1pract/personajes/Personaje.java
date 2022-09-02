007fbaa2843962d0e2ce448fb73178f6d013e305
Erick Manuel Mireles Fong <112106320+38195249@users.noreply.github.com>
2022-09-02 12:24:46 -0500

    Practica05 11:37 Finalizada
    Resubi la carpeta ya que el anterior commit solo se mostraba la carpeta de las clase

---
+++ practica05/src/com/poo1pract/personajes/Personaje.java
@@ -0,0 +1,30 @@
+package com.poo1pract.personajes;
+public class Personaje{
+
+    private String nombre;
+    private int edad;
+    public Personaje(String nombre) {
+        this.nombre = nombre;
+        int edad = 0;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void saludar(){
+        System.out.println("Hola alumno de POO " + nombre);
+    }
+    public boolean setEdad(int edad){
+        if (edad > 0 && edad <= 120){
+            this.edad = edad;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public int getEdad(){
+        return edad;
+    }
+}

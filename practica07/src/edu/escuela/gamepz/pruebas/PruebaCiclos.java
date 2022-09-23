2740fbc269b01552f5b78b9e9ad604155b66e9f8
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-09-23 11:38:44 -0500

    Practica07 11:38

--- practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
+++ practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -2,14 +2,23 @@ package edu.escuela.gamepz.pruebas;
 import edu.escuela.gamepz.personajes.Personaje;
 
 public class PruebaCiclos {
-    static int n = 0;
-    static int m = 0;
+    int n = 0;
+    int m = 0;
 
     public static void main(String[] args){
         Personaje per01 = new Personaje("Diego");
         Personaje per02 = new Personaje("Almendra");
         Personaje per03 = new Personaje("Blanca");
         Personaje per04 = new Personaje("Jordan");
+
+        while (n >= m){
+            n = (int) (Math.random()*100);
+            m = (int) (Math.random()*100);
+        }
+        this.n = n;
+        this.m = m;
+        System.out.println("n = " +n);
+        System.out.println("m = " +m);
 
         for (int i = n; i < m; i++){
             int ed = 0;
@@ -52,17 +61,6 @@ public class PruebaCiclos {
                 }
             }while (per04.setEdad(ed) == false);
             per04.getDetalle();
-        }
-    }
-    public void setVariables(int n, int m){
-            
-        while (n >= m){
-            n = (int) (Math.random()*100);
-            m = (int) (Math.random()*100);
-        }
-        this.n = n;
-        this.m = m;
-        System.out.println("n = " +n);
-        System.out.println("m = " +m);
+        }continue outfor;
     }
 }

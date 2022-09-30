dd36e7fe5590e93af7f46c78b28098940c266365
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-09-30 10:46:44 -0500

    Parcial01 10:46

--- parcial01/src/mx/parc1/soft/series/Serie.java
+++ parcial01/src/mx/parc1/soft/series/Serie.java
@@ -9,20 +9,22 @@ public class Serie {
     public int genNumero(int n){
         while (n < 100){
             n = (int)(Math.random()*x*10);
-
+            System.out.println("Numero generado = "+ n);
             if (n % 2 == 0){
+                System.out.println("Numero es par = "+ n);
                 this.n = n; 
+            }else{
+                System.out.println("Numero no es par = "+ n);
             }
         }
         return n;
     }
-    public int primos(int n){
+    public String primos(int n){
         for (int i = 0; i == n; i++){
             if (i % 2 == 0){
-                System.out.println(i + " ");
+               return System.out.println(i + "caca");
             }
         }
-        return n;
     }
     
 }

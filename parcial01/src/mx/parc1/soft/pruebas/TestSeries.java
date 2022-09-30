adf5b7dac1e20ddd758155ec4c9993fdd4a32a17
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-09-30 09:22:26 -0500

    Parcial01 9:21

--- parcial01/src/mx/parc1/soft/pruebas/TestSeries.java
+++ parcial01/src/mx/parc1/soft/pruebas/TestSeries.java
@@ -1,10 +1,10 @@
-package mx.soft.parc1.series;
-import mx.soft.parc1.parcial.Serie;
+package mx.parc1.soft.pruebas;
+import mx.parc1.soft.series.Serie;
 public class TestSeries{
     public static void main(String[] args){
         int x = 100;
         Serie s = new Serie(x);
-        int m = s.genNumero((Math.random()*x*10));
+        int m = s.genNumero(x);
         s.primos(m);
     }
 }
--- parcial01/src/mx/parc1/soft/series/Serie.java
+++ parcial01/src/mx/parc1/soft/series/Serie.java
@@ -1,11 +1,13 @@
-package mx.soft.parc1.parcial;
+package mx.parc1.soft.series;
 
 public class Serie {
+    int x = 100;
     int num;
     public Serie(int num){
         this.num = num;
     }
     public void genNumero(int num){
+        num = (int)(Math.random()*x*10);
         while (num < 100){
             this.num = num;
         }

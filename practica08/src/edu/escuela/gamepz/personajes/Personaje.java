a198ea196be6383daf6a0105310454d9faa345c8
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-10-07 08:15:21 -0500

    Practica08 8:15

--- practica08/src/edu/escuela/gamepz/personajes/Personaje.java
+++ practica08/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -2,10 +2,15 @@ package edu.escuela.gamepz.personajes;
 public class Personaje{
 
     private String nombre;
-    private int edad = 0;
+    private int vida = 0;
     
-    public Personaje(String nombre) {
+    public Personaje(String nombre, int vida) {
         this.nombre = nombre;
+        this.vida = vida;
+    }
+    public Personaje(String nombre){
+        this.nombre = nombre;
+        int vida = 3;
     }
     public void setNombre(String nombre){
         if (nombre.length() > 5 && nombre.length() < 25){
@@ -18,18 +23,24 @@ public class Personaje{
     public String getNombre(){
         return nombre;
     }
-    public boolean setEdad(int edad){
-        if (edad > 0 && edad <= 120){
-            this.edad = edad;
+    public boolean setVida(int vida){
+        if (vida > 0 && vida <= 99){
+            this.vida = vida;
             return true;
         }else{
             return false;
         }
     }
     public int getEdad(){
-        return edad;
+        return vida;
     }
     public void getDetalle(){
-        System.out.println(nombre + '\t' + edad);
+        System.out.println(nombre + '\t' + vida);
+    }
+    public void decVida(){
+
+    }
+    public void decVida(int a){
+
     }
 }

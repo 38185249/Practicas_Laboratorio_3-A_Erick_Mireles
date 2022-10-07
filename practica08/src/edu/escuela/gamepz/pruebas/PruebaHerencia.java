ebf2e0d506a7f8594c3be6cf899e48a46e293241
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-10-07 11:17:22 -0500

    Practica08 11:15

--- practica08/src/edu/escuela/gamepz/pruebas/PruebaHerencia.java
+++ practica08/src/edu/escuela/gamepz/pruebas/PruebaHerencia.java
@@ -18,8 +18,16 @@ public class PruebaHerencia {
         Zombie zom03 = new Zombie("Eduardo");
         
         Personaje [] personajes = {per01, per02, plan01, plan02, plan03, plan04, zom01, zom02, zom03};
-        for (Personaje aPer : personajes) {
+        for (Personaje a : personajes) {
             int aleat = (int) (Math.random()*100);
+            if (a instanceof Planta){
+                Planta tmp = (Planta) a;
+                System.out.println("Soy planta" + tmp.getEscudo());
+                tmp.setVida(aleat);
+            }
+            if (a instanceof Zombie){
+                
+            }
         }
     }
 }

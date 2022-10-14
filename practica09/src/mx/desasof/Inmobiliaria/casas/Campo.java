26a6f8826c71c1564702bc6f6db1f359f8fb15e4
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-10-14 08:57:19 -0500

    Practica09 8:57

--- practica09/src/mx/desasof/Inmobiliaria/casas/Campo.java
+++ practica09/src/mx/desasof/Inmobiliaria/casas/Campo.java
@@ -2,11 +2,21 @@ package mx.desasof.Inmobiliaria.casas;
 
 public class Campo extends Casa{
     
-    float piscina [];
-    
-    public Campo(String tipo, int hab, float costo) {
-        super(tipo, hab, costo);
-        
-    }
+    private float piscina[];
 
+    public Campo(String tipo, int hab, float costo, float ancho, float largo) {
+        super(tipo, hab, costo);
+        piscina[0] = ancho;
+        piscina[1] = largo;
+    }
+    public int hashCode(){
+        return ((super.hashCode()))/5;
+    }
+    public boolean equals(Campo objeto){
+        boolean result = false;
+        if ((super.equals(objeto)) && objeto instanceof Campo){
+            return result = true;
+        }
+        return result;
+    }
 }
--- practica09/src/mx/desasof/Inmobiliaria/casas/Casa.java
+++ practica09/src/mx/desasof/Inmobiliaria/casas/Casa.java
@@ -16,13 +16,18 @@ public class Casa {
         this.hab = 2;
         this.costo = 200000;
     }
+    public String toString(){
+        return tipo + '\t' + hab * '\t' + costo;
+    }
     public int hashCode(){
-        return (tipo.hashCode() * hab * costo);
+        return (int) (tipo.hashCode() * hab * costo);
     }
     public boolean equals(Casa objeto){
         boolean result = false;
         if ((objeto != null) && (objeto instanceof Casa)){
-            
+            if ((tipo == objeto.tipo) && (costo == objeto.costo)){
+                return result = true;
+            }
         }
         return result;
     }

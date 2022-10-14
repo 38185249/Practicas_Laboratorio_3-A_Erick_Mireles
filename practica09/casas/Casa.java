1913e3de5462f9749a125a065d7cccde4c604109
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-10-14 07:47:34 -0500

    Practica09 7:45 Inicio

---
+++ practica09/casas/Casa.java
@@ -0,0 +1,19 @@
+package practica09.casas;
+
+public class Casa {
+    
+    private String tipo;
+    private int hab;
+    private float costo; 
+
+    public Casa(String tipo, int hab, float costo){
+        this.tipo = tipo;
+        this.hab = hab;
+        this.costo = costo;
+    }
+    public Casa(String tipo){
+        this.tipo = tipo;
+        hab = ;
+        costo = 200000
+    }
+}

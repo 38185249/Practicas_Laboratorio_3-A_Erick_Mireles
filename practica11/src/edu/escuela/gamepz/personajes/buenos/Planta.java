2fefa50bda78215ae57c3500c0a0627266326a77
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-10-28 10:20:23 -0500

    Practica11 10:32

--- practica11/src/edu/escuela/gamepz/personajes/buenos/Planta.java
+++ practica11/src/edu/escuela/gamepz/personajes/buenos/Planta.java
@@ -28,7 +28,7 @@ public class Planta extends Personaje{
         this.escudo = Escudo.NULO;
     }
     public String toString(){
-        return super.toString() + '\t' + this.escudo;
+        return super.toString() + '\t' + escudo.getNivel();
     }
     public void decVida(){
         if (vida > 0){

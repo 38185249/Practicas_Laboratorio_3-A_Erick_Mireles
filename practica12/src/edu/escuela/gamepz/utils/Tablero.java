5f88c776705e342a044c575fea1951b1d98965ff
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-11-04 11:43:23 -0600

    Practica12 11:40

--- practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -24,17 +24,17 @@ public class Tablero {
     }
     public static void insertar(Personaje p, int pos) throws PersException{
         if (pos < 0 && pos > MAX_SIZE){
-            throw new PersException("Indice fuera de rango ", pos);
+            throw new PersException("Indice fuera de rango en ", pos);
         }else if(pos >= 0 && pos < MAX_SIZE){
             personajes[pos] = p;
         }
     }
     public static void borrar(int pos) throws PersException{
         if (pos < 0 && pos > MAX_SIZE){
-            throw new PersException("Indice fuera de rango ", pos);
-        }else if(personajes[pos] == null){
-            throw new PersException("Si personaje para borrar ", pos);
-        }else{
+            throw new PersException("Indice fuera de rango en ", pos);
+        }else if(pos < 0 && pos > MAX_SIZE && personajes[pos] == null){
+            throw new PersException("Si personaje para borrar en ", pos);
+        }else if (pos >= 0 && pos < MAX_SIZE){
             personajes[pos] = null;
         }
     }

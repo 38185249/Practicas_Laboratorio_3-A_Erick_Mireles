913bbb3b469d141234bb753e642741fc8d2d2ac8
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-11-04 10:35:45 -0600

    Practica12 10:35

--- practica12/src/edu/escuela/gamepz/pruebas/PruebaTablero.java
+++ practica12/src/edu/escuela/gamepz/pruebas/PruebaTablero.java
@@ -3,7 +3,7 @@ package edu.escuela.gamepz.pruebas;
 import edu.escuela.gamepz.personajes.Personaje;
 import edu.escuela.gamepz.personajes.buenos.Planta;
 import edu.escuela.gamepz.personajes.malos.Zombie;
-import edu.escuela.gamepz.utils.Escudo;
+import edu.escuela.gamepz.utils.Tablero;
 
 public class PruebaTablero {
     public static void main(String[] args) {
@@ -11,13 +11,22 @@ public class PruebaTablero {
         Personaje[] datos = {
             new Planta("David",100),
             new Zombie("Bianca"),
-        new Planta("Fabian",10,MEDIO),
+        new Planta("Fabian",10,Escudo.MEDIO),
         new Planta("Almendra",50),
-        new Planta("Ricardo",BAJO),
+        new Planta("Ricardo",Escudo.BAJO),
         new Planta("Silvia"),
         new Zombie("Armando",80,false),
         new Zombie("Josseline",true),
         new Zombie("Eduardo")
         };
+        for (Personaje p : datos) {
+            int pos = (int) (Math.random()*20-5);
+            try {
+                Tablero.insertar(p,pos);
+
+            } catch (PersException e) {
+                System.err.println(e.getMessage() + " " + e.getPos());
+            }
+        }
     }
 }

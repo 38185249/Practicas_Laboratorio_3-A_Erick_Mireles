190d894f0c5a381e3aa52a1ac3029fcdc0646acc
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-11-11 12:05:54 -0600

    Practica13 12:05

--- practica13/src/edu/escuela/gamepz/utils/ByVida.java
+++ practica13/src/edu/escuela/gamepz/utils/ByVida.java
@@ -1,5 +1,18 @@
 package edu.escuela.gamepz.utils;
 
-public class ByVida {
-    
+import java.util.Comparator;
+import edu.escuela.gamepz.personajes.Personaje;
+
+public class ByVida implements Comparator<Personaje>{
+
+    public int compare(Personaje p1, Personaje p2) {
+        if(p1.getVida() != p2.getVida()){
+            return p2.getVida() - p1.getVida();
+        } 
+        if (p1.getSize() != p2.getSize()){
+			return (p2.getSize()<p1.getSize())? -1:1;
+		}
+		return p1.getNombre().compareTo(p2.getNombre());
+    }
+
 }

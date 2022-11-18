5ec4cbf1d079e3b42b024388dd3da4e7787aa4fb
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-11-18 09:46:48 -0600

    Practica14 9:45 inicio

---
+++ practica14/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -0,0 +1,83 @@
+package edu.escuela.gamepz.personajes;
+
+import java.text.DecimalFormat;
+
+public abstract class Personaje implements Comparable<Personaje>{
+
+    private String nombre;
+    protected int vida = 0;
+    private float size;
+    
+    public Personaje(String nombre, int vida, float size) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.size = size;
+        if (size == 0.0f){
+            this.size = genSize();
+        }else{
+            this.size = Math.round(size)/100;
+        }
+    }
+    public Personaje(String nombre){
+        this.nombre = nombre;
+        this.vida = 3;
+        this.size = 0.0f;
+    }
+    public void setNombre(String nombre){
+        if (nombre.length() > 5 && nombre.length() < 25){
+            System.out.println("Nombre validado = " + nombre);
+            this.nombre = nombre;
+        }else{
+            System.out.println("Nombre invalido = " + nombre);
+        }
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public boolean setVida(int vida){
+        if (vida > 0 && vida <= 99){
+            this.vida = vida;
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public int getVida(){
+        return vida;
+    }
+    public String toString(){
+        return nombre + '\t' + vida + '\t' + size;
+    }
+    public abstract void decVida();
+   
+    public abstract void decVida(int a);
+   
+    public abstract void addVida();
+   
+    public abstract void addVida(int a);
+
+    public float genSize(){
+        float size = (float) (Math.random()*10+1);
+        DecimalFormat df = new DecimalFormat("#.00");
+        size = Float.valueOf(df.format(size));
+        return size;
+    }
+    public float getSize() {
+        return size;
+    }
+    public void setSize(float size) {
+        this.size = size;
+    }
+    public int compareTo(Personaje o) {
+        if(this.nombre.compareTo(o.nombre) !=0){
+			return this.nombre.compareTo(o.nombre);
+		}
+		if(this.vida != o.vida){
+			return o.vida - this.vida;
+		}
+		if(this.size == o.size){
+			return 0;
+		}
+		return (o.size < this.size)? -1 : 1;
+    }
+}
---
+++ practica14/src/edu/escuela/gamepz/personajes/buenos/Planta.java
@@ -0,0 +1,65 @@
+package edu.escuela.gamepz.personajes.buenos;
+
+import edu.escuela.gamepz.personajes.Personaje;
+import edu.escuela.gamepz.utils.Escudo;
+
+public class Planta extends Personaje{
+    private Escudo escudo;
+
+    public Planta(String nombre, int vida, Escudo escudo){
+        super(nombre, vida, 0.0f);
+        this.escudo = escudo;
+    }
+    public Planta(String nombre, Escudo escudo){
+        super(nombre);
+        super.setVida(3);
+        this.escudo = escudo;
+    }
+    public Planta(String nombre, int vida){
+        super(nombre, vida, 0.0f);
+        this.escudo = Escudo.NULO;
+    }
+    public Planta(String nombre){
+        super(nombre);
+        super.setVida(3);   
+        this.escudo = Escudo.NULO;
+    }
+    public String toString(){
+        return super.toString() + '\t' + escudo.getNivel();
+    }
+    public Escudo getEscudo(){
+        return escudo;
+    }
+    public void decVida(){
+        if (vida > 0){
+            this.vida -= (1 + escudo.getNivel());
+            if (vida < 0){
+                this.vida = 1;
+            }
+        } 
+    }
+    public void decVida(int a){
+        if (vida > 0){
+            this.vida -= (1 + (escudo.getNivel()) * a);
+            if (vida < 0){
+                this.vida = 1;
+            }
+        }
+    }
+    public void addVida(){
+        if (vida < 99){
+            this.vida += (1 + escudo.getNivel());
+            if (vida > 99){
+                this.vida = 99;
+            }
+        }
+    }
+    public void addVida(int a){
+        if (vida < 99){
+            this.vida += (1 + (escudo.getNivel()) * a);
+            if (vida > 99){
+                this.vida = 99;
+            }
+        }
+    }
+}

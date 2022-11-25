4f52ee8ae12be81ebf83b378efe2aa5d419188ce
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-11-25 10:00:47 -0600

    Practica15 10:00

--- practica15/src/mx/com/hilos/corredores/hilos/Letra.java
+++ practica15/src/mx/com/hilos/corredores/hilos/Letra.java
@@ -13,6 +13,16 @@ public class Letra implements Runnable{
 
     public void run() {
         String cadena = etq.getText();
-
+        
+        for(int i = 0; i < 51; i++){
+            cadena += letra;
+            etq.setText(cadena);
+            int rand = (int) (Math.random()* 1000 + 1);
+            try {
+                Thread.sleep(rand);
+            } catch (InterruptedException e) {
+                System.out.println("Error al intentar dormir el hilo: " + e);
+            }
+        }
     }
 }

a80ed6e3f1a9dcdadb31978384e372bc4b664805
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-11-25 09:15:43 -0600

    Practica15 9:15

--- practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -8,8 +8,17 @@ public class RunHilos {
         char ch1 = txt1.getText().charAt(0);
         char ch2 = txt2.getText().charAt(0);
 
-        if(txt1 == null && txt2 == null){
+        if(txt1.getText() == null){
             txt1.setText("E");
         }
+        if(txt2.getText() == null){
+            txt2.setText("M");
+        }
+
+        String salida1 = String.valueOf(ch1);
+        String salida2 = String.valueOf(ch2);
+
+        elem1.setText(salida1);
+        elem2.setText(salida2);
     }
 }

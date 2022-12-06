f033201d66d65680908eb54fb434f3b8534178a0
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-12-06 10:53:21 -0600

    Ordinario 10:53 Paso 8, 9 y 10

--- Ordinario/src/ex/ordinario/www/eval/acciones/PorCosto.java
+++ Ordinario/src/ex/ordinario/www/eval/acciones/PorCosto.java
@@ -1,3 +1,16 @@
-public class PorCosto {
-    
+package ex.ordinario.www.eval.acciones;
+
+import java.util.Comparator;
+import ex.ordinario.www.eval.vehiculos.Vehiculo;
+
+public class PorCosto implements Comparator<Vehiculo>{
+    public int compare(Vehiculo p1, Vehiculo p2) {
+        if (p1.getCosto() != p2.getCosto()){
+            return (p1.getCosto()<p2.getCosto())? -1:1;
+        }
+        if(p1.getModelo() != p2.getModelo()){
+            return p2.getModelo() - p1.getModelo();
+        } 
+        return p1.getMarca().compareTo(p2.getMarca());
+    }
 }
--- Ordinario/src/ex/ordinario/www/eval/acciones/PorMarca.java
+++ Ordinario/src/ex/ordinario/www/eval/acciones/PorMarca.java
@@ -1,8 +1,16 @@
 package ex.ordinario.www.eval.acciones;
 
 import java.util.Comparator;
+import ex.ordinario.www.eval.vehiculos.Vehiculo;
 
 public class PorMarca implements Comparator<Vehiculo>{
-
-
+    public int compare(Vehiculo p1, Vehiculo p2) {
+        if(p1.getModelo() != p2.getModelo()){
+            return p2.getModelo() - p1.getModelo();
+        } 
+        if (p1.getCosto() != p2.getCosto()){
+            return (p2.getCosto()<p1.getCosto())? -1:1;
+		}
+        return p1.getMarca().compareTo(p2.getMarca());
+    }
 }

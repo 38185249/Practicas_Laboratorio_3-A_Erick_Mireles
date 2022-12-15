8431df889e72bd5d24ac9fdd8c96b8a5b58f0b5a
Erick Manuel Mireles Fong <38195249@uaz.edu.mx>
2022-12-15 09:06:45 -0600

    Extra 9:06 paso 9 y 10

--- Extraordinario/src/mx/edu/elextra/extraeval/acciones/PorCosto.java
+++ Extraordinario/src/mx/edu/elextra/extraeval/acciones/PorCosto.java
@@ -1,3 +1,14 @@
-public class PorCosto{
+package mx.edu.elextra.extraeval.acciones;
+
+import java.util.Comparator;
+import mx.edu.elextra.extraeval.dispositivos.Dispositivo;
+
+public class PorCosto implements Comparator<Dispositivo>{
+    public int compare(Dispositivo o1, Dispositivo o2) {
+        if (o1.getCosto() != o2.getCosto()){
+            return (o1.getCosto()<o2.getCosto())? -1:1;
+        }
+        return o1.getMarca().compareTo(o2.getMarca());
+    }
 
 }
--- Extraordinario/src/mx/edu/elextra/extraeval/acciones/PorMarca.java
+++ Extraordinario/src/mx/edu/elextra/extraeval/acciones/PorMarca.java
@@ -1,5 +1,16 @@
 package mx.edu.elextra.extraeval.acciones;
 
-public class PorMarca{
-    
+import java.util.Comparator;
+import mx.edu.elextra.extraeval.dispositivos.Dispositivo;
+
+public class PorMarca implements Comparator<Dispositivo>{
+    public int compare(Dispositivo o1, Dispositivo o2) {
+        if(o1.getMarca().compareTo(o2.getMarca()) != 0){
+			return o1.getMarca().compareTo(o2.getMarca());
+        }
+        if (o1.getCosto() == o2.getCosto()){
+            return 0;
+        }
+        return (o2.getCosto()<o1.getCosto())? -1:1;
+    }
 }
